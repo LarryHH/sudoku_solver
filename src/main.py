254b63ee1fcdbeff1dc91a09836ee9ad7bc8458d
Larry <larryhieuhuynh@gmail.com>
2021-01-07 21:00:20 +0800

    more stuff to recursion logic

--- src/main.py
+++ src/main.py
@@ -1,10 +1,9 @@
 import numpy as np
 import random
 
-
 class Board:
     def __init__(self, difficulty):
-        self.board = [[0 for c in range(9)] for cc in range(9)]
+        self.board = np.array([[0 for c in range(9)] for cc in range(9)])
         self.difficulty = difficulty
         self.initBoardDiagonal(0)
 
@@ -28,29 +27,68 @@ class Board:
         if(start < 6):
             self.initBoardDiagonal(start+3)
 
+
+    def attemptInsert(self, r, c, num):
+        r1, r2, c1, c2 = self.checkTridrant(r, c)
+        if (num in self.board[r,:]) | (num in self.board[:,c]):
+            return False
+        elif (num in self.board[r1:r2,c1:c2]):
+            return False
+        else:
+            self.board[r][c] = num
+            return True
+
+
     def boardSolver(self, row, col):
-        # TODO: recursion for solver
+        print(row, col)
         if ((col == len(self.board)-1) and (row == len(self.board)-1)): # end of board (col == 8, row == 8) 
             print("end of board")
             return True
-        elif (col == len(self.board)-1): # end of row (col = 8) 
+        elif (col > len(self.board)-1): # end of row (col = 8) 
             return self.boardSolver(row+1, 0)
         elif (self.board[row][col] != 0): # non-empty cell
             return self.boardSolver(row, col+1)   
         else: #empty cell
-            # attemptInsert()
-            return self.boardSolver(row, col+1)
-
+            # TODO: this recursion doesn't work, need to figure this one out
+            for num in list(range(1,10)):
+                if (self.attemptInsert(row, col, num)):
+                    return self.boardSolver(row, col+1)
+                else:
+                    continue
 
     def printBoard(self):
         [print(self.board[c]) for c in range(len(self.board))]
 
-if __name__ == "__main__":
+    def checkTridrant(self, row, col):
+        row_tridrant = row/3
+        col_tridrant = col/3
+        if row_tridrant < 1 and col_tridrant < 1:
+            return (0, 3, 0, 3)
+        if row_tridrant < 1 and col_tridrant < 2:
+            return (0, 3, 3, 6)
+        if row_tridrant < 1 and col_tridrant < 3:
+            return (0, 3, 6, 9)
+        if row_tridrant < 2 and col_tridrant < 1:
+            return (3, 6, 0, 3)
+        if row_tridrant < 2 and col_tridrant < 2:
+            return (3, 6, 3, 6)
+        if row_tridrant < 2 and col_tridrant < 3:
+            return (3, 6, 6, 9)                        
+        if row_tridrant < 3 and col_tridrant < 1:
+            return (6, 9, 0, 3)
+        if row_tridrant < 3 and col_tridrant < 2:
+            return (6, 9, 3, 6)
+        if row_tridrant < 3 and col_tridrant < 3:
+            return (6, 9, 6, 9)
 
+if __name__ == "__main__":
+    # TODO: recursion for solver
     # TODO: difficulty affects how many cells removed at init
     # TODO: attach "holding" cells to board, maybe duplicate grid
+
     difficulty = ["Easy", "Moderate", "Hard"]
     #init board
+
     board = Board(difficulty[0])
-    board.printBoard()
     board.boardSolver(0, 0)
+    board.printBoard()
